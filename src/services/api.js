e813847bb89cfdc50c821e998df5cd66bbfcf7f6
JITHIN LAKSHMAN <158822916+Jithin15P@users.noreply.github.com>
2026-03-12 13:06:51 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,13 @@
 import axios from 'axios';
- 
+
 const api = axios.create({
-  
-  baseURL: 'https://stock-trading-backend-nu.vercel.app/', 
+  baseURL: 'https://stock-trading-backend-gi24.onrender.com',  // FIXED
 });
 
- 
 api.interceptors.request.use(
   (config) => {
-    
     const token = localStorage.getItem('token');
- 
+
     if (token) {
       config.headers['Authorization'] = `Bearer ${token}`;
     }
